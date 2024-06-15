0b63bd04207dff97521847772db5b5a1fa5da05d
yuancheerup <1498175161@qq.com>
2024-06-15 21:44:37 +0800

    add elment-plus组件的汉化(该组件默认是英文) modify UserPage中的bug

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import router from './router'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import '@/assets/css/global.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import ElementPlus from 'element-plus'
 
 const app = createApp(App)
 
@@ -13,5 +15,9 @@ app.use(router)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
+
+app.use(ElementPlus, {
+  locale: zhCn
+})
 
 app.mount('#app')
